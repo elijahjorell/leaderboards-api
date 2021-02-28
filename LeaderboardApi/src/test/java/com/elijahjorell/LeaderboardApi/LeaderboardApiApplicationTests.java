d4e97a28bd16f8d282be339854c6955db29bc23c
elijahjorell <elijahesmero@gmail.com>
2021-03-01 09:47:45 +1100

    Import spring intializr project

---
+++ LeaderboardApi/src/test/java/com/elijahjorell/LeaderboardApi/LeaderboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elijahjorell.LeaderboardApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
